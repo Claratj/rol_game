127709babbfc9064b9297df1f88f0793651ede0a
Clara <clara@claratoloba.com>
2021-03-26 18:57:53 +0100

    text battle ok

--- ex-6.js
+++ ex-6.js
@@ -13,12 +13,78 @@ winPlayer.classList.add('ml15');
 let player1Game = [];
 let table = document.createElement('div');
 let pOne = document.createElement('p');
+pOne.classList.add('p-battle');
 let pTwo = document.createElement('p');
+pTwo.classList.add('p-battle');
 let divOne = document.createElement('div');
 let imgOne = document.createElement('div');
 let divTwo = document.createElement('div');
 let imgTwo = document.createElement('div');
 
+let p1 = [];
+let p2 = [];
+
+let text='';
+let z=0;
+
+function typeWriter() {
+ 
+    if ((z < text1.length) || (z < text2.length))  {
+        pOne.innerHTML += text1.charAt(z);
+        pTwo.innerHTML += text2.charAt(z);
+        z++;
+        setTimeout(typeWriter, 100);
+    }
+}
+
+function delay(i) {
+    
+    text1=p1[i];
+    text2=p2[i];
+    z=0;
+    w=0;
+    pOne.innerHTML="";
+    pTwo.innerHTML="";
+    
+    typeWriter();
+    
+    
+    //Efectos ataque
+    if(i<p1.length-1){ 
+        if (i%2==0){
+            // console.log("Ronda" + i +"jugador1 atacando");
+        }else{
+            // console.log("Ronda" + i +"jugador2 atacando");
+        }
+        setTimeout(function(){
+            delay(i+1);;
+        }, 6000);
+    }else{
+        
+        //Ganador y botón de jugar de nuevo
+        document.body.appendChild(winner);
+        winner.appendChild(winPlayer);
+        refreshBtn.innerHTML= "Let's Play Again!";
+        document.body.appendChild(refreshBtn);
+        refreshBtn.addEventListener('click', () => {startAgain()});
+        
+        //Efecto Winner
+        anime.timeline({loop: true}).add({
+            targets: '.ml15 .word',
+            scale: [14,1],
+            opacity: [0,1],
+            easing: "easeOutCirc",
+            duration: 600,
+            delay: (el, i) => 800 * i
+        }).add({
+            targets: '.ml15',
+            opacity: 0,
+            duration: 600,
+            easing: "easeOutExpo",
+            delay: 800
+        });
+    }
+}
 
 const intro= () => {
     const splash = document.querySelector('.splash');
@@ -27,11 +93,11 @@ const intro= () => {
         splash.className = 'none';
         console.log(splash);
     }, 2000);
-
+    
 }
 
 const showCharacters = () => {
-
+    
     fetch('http://localhost:3000/characters').then(res => res.json()).then(characters => {
         
         const div$$ = document.querySelector('.main-container');
@@ -40,38 +106,38 @@ const showCharacters = () => {
             
             let card = document.createElement('div'); //Contenedor negro
             card.classList.add('card');
-
+            
             let cardSubBackground = document.createElement('div'); // Contenedor verde
             cardSubBackground.classList.add('green-part');
-
+            
             let cardFrame = document.createElement('div'); // Contenedor gris
             cardFrame.classList.add('card-frame');
-
+            
             let frameHeader = document.createElement('div'); //Contenedor Nombre
             frameHeader.classList.add('frameHeader');
-
-
+            
+            
             let characterName = document.createElement('h3'); // Nombre
             characterName.classList.add('name');
             characterName.innerHTML = character.name;
-
+            
             let img$$ = document.createElement('img'); // Imagen
             img$$.setAttribute('src', character.avatar);
-
+            
             let characteristics = document.createElement('div'); // Texto descriptivo
             characteristics.classList.add('characteristics');
-
+            
             let damageChar = document.createElement('p'); 
             damageChar.innerHTML = '<strong>Damage:</strong> ' + character.damage;
-
+            
             let critic = document.createElement('p');
             critic.innerHTML = '<strong>Critic:</strong> '+ character.critic;
             let defense = document.createElement('p');
             defense.innerHTML = '<strong>Defense:</strong> '+ character.defense;
             let vitalityChar = document.createElement('p');
             vitalityChar.innerHTML ='<strong>Vitality:</strong> '+ character.vitality;
-
-
+            
+            
             card.appendChild(cardSubBackground);
             cardSubBackground.appendChild(cardFrame);
             cardFrame.appendChild(frameHeader);
@@ -82,7 +148,7 @@ const showCharacters = () => {
             characteristics.appendChild(critic);
             characteristics.appendChild(defense);
             characteristics.appendChild(vitalityChar);
-
+            
             div$$.appendChild(card);
             card.appendChild(cardSubBackground);
             cardSubBackground.appendChild(cardFrame);
@@ -98,9 +164,9 @@ const showCharacters = () => {
 
 
 function chooseFighters(fighter) {
-
+    
     if (charactersSelected.length < 2 ) {
-
+        
         charactersSelected.push(fighter);
         if (charactersSelected.length === 2) {
             playButton.innerHTML = 'Lets Play!';
@@ -108,18 +174,19 @@ function chooseFighters(fighter) {
             playButton.addEventListener('click', ()=>{ 
                 letsPlay()
             });
-    } 
+        } 
     }
 }
 
 function letsPlay () {
-
+    
     playButton.remove();
     pOne.innerHTML = '';
     pTwo.innerHTML = '';
     divOne.style.border = "0px";
     divTwo.style.border = "0px";
-
+    
+    
     //Jugando
     
     let player1 = charactersSelected[0];
@@ -130,13 +197,13 @@ function letsPlay () {
     let scorePlayer2 = [];
     let vitalityPlayer1 = [];
     let vitalityPlayer2 = [];
-
-   
+    
+    
     while(lifeVitalityP2 > 0 && lifeVitalityP1 > 0 ) {
         //Juega player1
         let score1 = playerTotalDamage(player1.damage) - player2.defense ;
         lifeVitalityP2 = lifeVitalityP2 - score1;
-
+        
         scorePlayer1.push(score1);
         vitalityPlayer2.push(lifeVitalityP2);
         
@@ -158,8 +225,8 @@ function letsPlay () {
     // Tablero de rondas
     table.classList.add('table');
     document.body.appendChild(table);
-
-
+    
+    
     //Div Player 1
     divOne.classList.add('fighter');
     imgOne.classList.add('fighter-img');
@@ -173,83 +240,43 @@ function letsPlay () {
     imgTwo.style.backgroundImage = `url('${charactersSelected[1].avatar}')`;
     divTwo.appendChild(imgTwo);
     // pTwo.classList.add('ml12');
-
+    
     
     //Print todo
     let x = 0;
     let j = 0;
-
-    let p1 = [];
-    let p2 = [];
     
-
+    
+    
     for (let index = 1; index < (scorePlayer1.length*2); index++) {
-            // console.log(index);
+        // console.log(index);
+        
+        if (index % 2 === 0 && index < (scorePlayer1.length*2)-1){
             
-            if (index % 2 === 0 && index < (scorePlayer1.length*2)-1){
-            
-                p2.push(`Ronda ${index} : Ataca infligiendo ${scorePlayer2[j]} de daño`);
-                p1.push(`Ronda ${index} : Su vitalidad se ve reducida a ${vitalityPlayer1[j]}.`);
-                j++;
-            } else if (index % 2 !== 0 && index < (scorePlayer1.length*2)-1) {
-                p2.push(`Ronda ${index} : Su vitalidad se ve reducida a ${vitalityPlayer2[x]}.`);
-                p1.push(`Ronda ${index} : Ataca infligiendo ${scorePlayer1[x]} de daño`);
-                x++;
-            } else {
-                if (vitalityPlayer1[j] <= 0) {
-                    p1.push(`El jugador fue derrotado durante la batalla.`);
-                    p2.push(`¡Vencedor de la batalla!`);
-                    // divTwo.style.border = "3px solid blue";
-                } else if (vitalityPlayer2[x] <= 0) {
-                    p2.push(`El jugador fue derrotado durante la batalla.`);
-                    p1.push(`¡Vencedor de la batalla!`);
-                    // divOne.style.border = "3px solid blue";
-                }
+            p2.push(`Ronda ${index} : Ataca infligiendo ${scorePlayer2[j]} de daño`);
+            p1.push(`Ronda ${index} : Su vitalidad se ve reducida a ${vitalityPlayer1[j]}.`);
+            j++;
+        } else if (index % 2 !== 0 && index < (scorePlayer1.length*2)-1) {
+            p2.push(`Ronda ${index} : Su vitalidad se ve reducida a ${vitalityPlayer2[x]}.`);
+            p1.push(`Ronda ${index} : Ataca infligiendo ${scorePlayer1[x]} de daño`);
+            x++;
+        } else {
+            if (vitalityPlayer1[j] <= 0) {
+                p1.push(`El jugador fue derrotado durante la batalla.`);
+                p2.push(`¡Vencedor de la batalla!`);
+                // divTwo.style.border = "3px solid blue";
+            } else if (vitalityPlayer2[x] <= 0) {
+                p2.push(`El jugador fue derrotado durante la batalla.`);
+                p1.push(`¡Vencedor de la batalla!`);
+                // divOne.style.border = "3px solid blue";
             }
         }
-        
-        
-        
-        function delay(i) {
-            pOne.innerHTML = p1[i];
-            pTwo.innerHTML = p2[i]
-            divOne.appendChild(pOne);
-            divTwo.appendChild(pTwo);
-        // console.log(p1[i]);
-        // console.log(p2[i]);
-        
-        if(i<p1.length-1){ 
-            setTimeout(function(){
-                delay(i+1)
-            }, 2000);
-        }else{
-
-            //Ganador y botón de jugar de nuevo
-            document.body.appendChild(winner);
-            winner.appendChild(winPlayer);
-            refreshBtn.innerHTML= "Let's Play Again!";
-            document.body.appendChild(refreshBtn);
-            refreshBtn.addEventListener('click', () => {startAgain()});
-        
-            //Efecto Winner
-            anime.timeline({loop: true}).add({
-            targets: '.ml15 .word',
-            scale: [14,1],
-            opacity: [0,1],
-            easing: "easeOutCirc",
-            duration: 600,
-            delay: (el, i) => 800 * i
-          }).add({
-            targets: '.ml15',
-            opacity: 0,
-            duration: 600,
-            easing: "easeOutExpo",
-            delay: 800
-          });
-        }
-      }
-      
-      delay(0)
+    }
+    
+    divOne.appendChild(pOne);
+    divTwo.appendChild(pTwo);
+    
+    delay(0)
     // console.log(p1);
     
     //-------------------------------------------------------------------
